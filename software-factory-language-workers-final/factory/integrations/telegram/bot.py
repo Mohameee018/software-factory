6c8f9685e135c9ee1ba2d728c88d1db57f937b9d
Mohamed Ahmed <midoiq535@gmail.com>
2026-09-26 02:27:13 +0300

    feat: route Telegram photo references to UIUX workflow

--- software-factory-language-workers-final/factory/integrations/telegram/bot.py
+++ software-factory-language-workers-final/factory/integrations/telegram/bot.py
@@ -14,6 +14,7 @@ class TelegramBot:
         self.application.add_handler(CommandHandler('start',self._guard(h.start))); self.application.add_handler(CommandHandler('help',self._guard(h.help))); self.application.add_handler(CommandHandler('new',self._guard(h.new)))
         for cmd,fn in [('projects',h.projects),('project',h.project),('status',h.status),('tasks',h.tasks),('run',h.run),('pause',h.pause),('resume',h.resume),('cancel',h.cancel),('retry',h.retry),('logs',h.logs),('approve',h.approve),('reject',h.reject),('review',h.review),('feedback',h.feedback)]: self.application.add_handler(CommandHandler(cmd,self._guard(fn)))
         self.application.add_handler(CallbackQueryHandler(h.callback,pattern=r'^apr:[ar]:'))
+        self.application.add_handler(MessageHandler(filters.PHOTO,self._guard(h.photo)))
         self.application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND,self._guard(h.text)))
     def _guard(self, fn):
         async def wrapped(update, context):
